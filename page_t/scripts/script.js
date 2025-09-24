c8b2c27a6c7ac065c71af2c39bedb8c6d14848e4
fuji53 <damirsultanov3300@gmail.com>
2025-09-25 00:39:58 +0500

    new additions!

--- page_t/scripts/script.js
+++ page_t/scripts/script.js
@@ -1,1 +1,18 @@
 alert("dont leave me alone");
+
+function themeToggler() {
+    let theme = localStorage.getItem('theme')
+    if (theme == null) {
+        theme = 'black'
+    } else if (theme === 'white') {
+        theme = 'black'
+    } else if (theme === 'black') {
+        theme = 'white'
+    }
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('theme', theme)
+}
+
+setInterval(function () {
+    document.getElementById('time').innerHTML = 'Server Time: ' + new Date().toLocaleString('ru-RU', {timeZone: 'Asia/Yekaterinburg'});
+}, 1000);
